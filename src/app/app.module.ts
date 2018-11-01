134170e9aec149e2ef9f6c559fd04869d85886dd
Matthieu Borgognon <matrash@bqn.ch>
2018-11-01 10:44:56 +0100

    Reactive Forms Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,9 @@ import { MatButtonModule} from '@angular/material/button';
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
 import { MatCheckboxModule } from "@angular/material/checkbox";
-import { FormsModule } from "@angular/forms";
+import { MatSelectModule } from "@angular/material/select";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppRoutingModule } from "./app-routing/app-routing.module";
 
@@ -59,7 +61,10 @@ import { MatDialogModule } from '@angular/material';
     MatFormFieldModule,
     MatInputModule,
     MatCheckboxModule,
+    MatSlideToggleModule,
+    MatSelectModule,
     FormsModule,
+    ReactiveFormsModule,
     FlexLayoutModule,
     AppRoutingModule
   ],
