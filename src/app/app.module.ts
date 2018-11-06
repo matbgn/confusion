158ebedce180739d3a548ae2699e039d7005f494
Matthieu Borgognon <matrash@bqn.ch>
2018-11-06 15:51:59 +0100

    Directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -42,6 +42,7 @@ import { LeaderService } from "./services/leader.service";
 import { ProcessHTTPMsgService } from "./services/process-httpmsg.service";
 
 import { baseURL } from './shared/baseurl';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
@@ -53,7 +54,8 @@ import { baseURL } from './shared/baseurl';
     AboutComponent,
     HomeComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
